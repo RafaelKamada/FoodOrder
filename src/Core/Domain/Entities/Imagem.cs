afebd0d7bcf379396bb71681cf7cf54c9466e010
Robson Vilaça <robsonvilaca.lima@gmail.com>
2024-10-12 18:47:38 -0300

    feature: novas entidades de produto

---
+++ src/Core/Domain/Entities/Imagem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Imagem
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required]
+        public byte[] Data { get; set; }
+
+        [Required] 
+        [StringLength(255)] 
+        public string Nome { get; set; } 
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [ForeignKey("ProdutoId")]
+        public Produto Produto { get; set; }
+    }
+
+}
