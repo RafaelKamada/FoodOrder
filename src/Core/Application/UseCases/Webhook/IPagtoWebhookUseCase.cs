09c37aa916e28c0c7bbfd726d9837474434f938a
RafaelKamada <rafael-kamada2007@hotmail.com>
2024-12-29 23:28:40 -0300

    feat(item-03): Iniciado webhook

---
+++ src/Core/Application/UseCases/Webhook/IPagtoWebhookUseCase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FoodOrder.Application.UseCases.Webhook
+{
+    public interface IPagtoWebhookUseCase
+    {
+        Task<string> AtualizarStatusPagamento(string webhook);
+    }
+}
---
+++ src/Core/Application/UseCases/Webhook/PagtoWebhookUseCase.cs
@@ -0,0 +1,13 @@
+﻿
+namespace FoodOrder.Application.UseCases.Webhook
+{
+    public class PagtoWebhookUseCase : IPagtoWebhookUseCase
+    {
+        public Task<string> AtualizarStatusPagamento(string webhook)
+        {
+
+            //TOD: implementar
+            throw new NotImplementedException();
+        }
+    }
+}
