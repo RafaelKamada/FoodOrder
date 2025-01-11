244d7991447c4f42becb647843b4c517b189dc15
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-01-11 20:50:30 -0300

    feat(item_03): Adicionado configs ao appSettings

--- src/Infrastructure/Infra.Data/External/MercadoPago/MercadoPagoExternalService.cs
+++ src/Infrastructure/Infra.Data/External/MercadoPago/MercadoPagoExternalService.cs
@@ -31,7 +31,7 @@ namespace FoodOrder.Data.External.MercadoPago
             try
             {
                 var accessToken = _configuration["MercadoPago:AccessToken"];
-                var url = "https://run.mocky.io/v3/61a20a25-1916-46d2-a59e-125e5992aabe";
+                var url = _configuration["MercadoPago:EndPoints:CriarPagamento"];
                 //var url = "https://api.mercadopago.com/v1/payments";
 
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
@@ -62,7 +62,7 @@ namespace FoodOrder.Data.External.MercadoPago
             try
             {
                 var accessToken = _configuration["MercadoPago:AccessToken"];
-                var url = $"https://run.mocky.io/v3/9dad049c-d48c-47f2-833d-a29ef385c9bb";
+                var url = _configuration["MercadoPago:EndPoints:DetalhesPagamento"];
                 //var url = $"https://api.mercadopago.com/v1/payments/{paymentId}";
 
                 _httpClient.DefaultRequestHeaders.Authorization =
