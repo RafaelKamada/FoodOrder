3292794daeb7e9c3b8a792d2af220ed3939f2032
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-20 19:21:06 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,76 +1,25 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-# Fiap Pos tech
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "DevopsFiap"
-
-    workspaces {
-      name = "gh-actions"
-    }
-  }
-}
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.regionDefault
 }
 
-resource "random_pet" "sg" {}
+resource "aws_security_group" "sg" {
+  name        = "SG-${var.projectName}"
+  description = "Security Group do Food Order API"
+  vpc_id      = aws_vpc.main_vpc.id
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "<style> body {background-color: black;}</style><img style="text-align:center" src="https://postech.fiap.com.br/gifs/loader.gif"><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+  # Inbound
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  # Outbound
   egress {
+    description = "All"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -78,6 +27,160 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnets[*].id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "172.31.0.0/16"
+
+  tags = {
+    Name        = "Main VPC"
+    Environment = "production"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = 2
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = cidrsubnet("172.31.0.0/16", 4, count.index + 2)
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+
+  tags = {
+    Name        = "Public Subnet ${count.index + 1}"
+    Environment = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count = 2
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = cidrsubnet("172.31.0.0/16", 4, count.index)
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+
+  tags = {
+    Name        = "Private Subnet ${count.index + 1}"
+    Environment = "private"
+  }
+}
+
+resource "aws_eks_access_policy_association" "eks-access-policy" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  policy_arn    = var.policyArn
+  principal_arn = var.principalArn
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_eks_access_entry" "eks-access-entry" {
+  cluster_name      = aws_eks_cluster.eks-cluster.name
+  principal_arn     = var.principalArn
+  kubernetes_groups = ["food-order-api-groups"]
+  type              = "STANDARD"
+}
+
+resource "aws_lb" "food_order_lb" {
+  name               = "food-order-lb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg.id]
+  subnets            = aws_subnet.private_subnets[*].id
+}
+
+resource "aws_lb_target_group" "food_order_tg" {
+  name     = "food-order-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.food_order_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.food_order_tg.arn
+  }
+}
+
+resource "aws_eks_node_group" "eks-node" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = var.nodeGroup
+  node_role_arn   = var.labRole
+  subnet_ids      = aws_subnet.private_subnets[*].id
+  disk_size       = 50
+  instance_types  = [var.instanceType]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = var.projectName
+  role_arn = var.labRole
+  
+  vpc_config {
+    subnet_ids         = aws_subnet.private_subnets[*].id
+    security_group_ids = [aws_security_group.sg.id]
+  }
+
+  access_config {
+    authentication_mode = var.accessConfig
+  }
+}
+
+resource "aws_api_gateway_rest_api" "food_order_api" {
+  name        = "food-order-api"
+  description = "API Gateway para o Food Order API"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+  parent_id   = aws_api_gateway_rest_api.food_order_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.food_order_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "load_balancer" {
+  rest_api_id             = aws_api_gateway_rest_api.food_order_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = "http://${aws_lb.food_order_lb.dns_name}"
+}
+
+resource "aws_api_gateway_deployment" "food_order_api" {
+  depends_on = [aws_api_gateway_integration.load_balancer]
+  rest_api_id = aws_api_gateway_rest_api.food_order_api.id
+  stage_name  = "prod"
 }
