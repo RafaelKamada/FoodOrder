6607a7b0cb67e75678e119b1c10a086483c0af6f
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-20 19:22:32 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,29 +3,6 @@ provider "aws" {
   region = var.regionDefault
 }
 
-resource "aws_security_group" "sg" {
-  name        = "SG-${var.projectName}"
-  description = "Security Group do Food Order API"
-  vpc_id      = aws_vpc.main_vpc.id
-
-  # Inbound
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound
-  egress {
-    description = "All"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "private_subnet_ids" {
   value = aws_subnet.private_subnets[*].id
