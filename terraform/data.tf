2749d39f19f4220b766775227d48eb4ed6573c19
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-22 17:09:09 -0300

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -30,3 +30,28 @@ resource "aws_subnet" "private_subnets" {
     Environment = "private"
   }
 }
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private_subnets[*].id)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
