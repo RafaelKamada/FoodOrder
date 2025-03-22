da66bfbf8bf0b145ffe082598a3e239dedcb9029
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-22 18:46:44 -0300

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "EKS API Server"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Outbound
   egress {
     description = "All"
