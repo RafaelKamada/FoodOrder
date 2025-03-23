2089eb9c079614ec9b19375b8c308d6563d1d774
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-23 11:44:03 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,10 @@ resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.eks-cluster.name
   addon_name   = "coredns"
   addon_version = "v1.11.4-eksbuild.2"
+
+  depends_on = [
+    aws_eks_node_group.eks-node
+  ]
 }
 
 resource "aws_eks_addon" "eks-pod-identity-agent" {
