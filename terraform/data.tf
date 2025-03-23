daadbfd1a5155009fea38f29d89a96ecd9dc4346
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-23 17:16:51 -0300

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -85,12 +85,6 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
-}
-
 resource "aws_route_table_association" "public_subnet_association" {
   count          = 2
   subnet_id      = aws_subnet.public_subnets[count.index].id
