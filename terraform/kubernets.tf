3f02049db09a74aa5a4085017fd4770419c6e81a
Rafael Kamada <43392619+RafaelKamada@users.noreply.github.com>
2025-03-24 21:34:36 -0300

    Update kubernets.tf

--- terraform/kubernets.tf
+++ terraform/kubernets.tf
@@ -38,7 +38,7 @@ resource "kubernetes_deployment" "api" {
 
         container {
           name  = "api-pod-config"
-          image = "vilacaro/api:v4.2"
+          image = "vilacaro/api:v4.3"
 
           port {
             container_port = 9000
